7e540fbc4ae9f09f48ccac0e7079de0c860941ed
瑄 <shanboxuan@me.com>
2022-05-23 20:26:44 +0100

    Refine ratios

--- utils.py
+++ utils.py
@@ -1,49 +1,69 @@
 import math
 import ifacial
 
-POSITION_X_RATIO = 100
-POSITION_Y_RATIO = 100
-POSITION_Z_RATIO = 20
+FACE_POSITION_X_RATIO = 100
+FACE_POSITION_Y_RATIO = 100
+FACE_POSITION_Z_RATIO = 20
+
+FACE_ANGLE_X_RATIO = 1
+FACE_ANGLE_Y_RATIO = 1
+FACE_ANGLE_Z_RATIO = 1
+
+MOUTH_SMILE_RATIO = 3
+MOUSE_OPEN_RATIO = 1.5
+
+BROWS_RATIO = 2
+
 TONGUE_OUT_RATIO = 10
-MOUSE_SMILE_RATIO = 2
-MOUSE_SMILE_OFFSET = 0.3
-MOUSE_OPEN_RATIO = 1
-EYE_OPEN_RATIO = 2
+
+EYE_OPEN_RATIO = 1.5
+EYE_ROTATION_RATIO = 1.5
+
+CHEEK_PUFF_RATIO = 2
+FACE_ANGRY_RATIO = 2
+
+BROW_LEFT_Y_RATIO = 2
+BROW_RIGHT_Y_RATIO = 2
+
+MOUTH_X_RATIO = 2
+
 
 def build_params_dict(ifacial_data):
     ifacial_data = [
         {
             "id": "FacePositionX",
-            "value": ifacial_data[ifacial.HEAD_POSITION_X] * POSITION_X_RATIO
+            "value": ifacial_data[ifacial.HEAD_POSITION_X] * FACE_POSITION_X_RATIO
         },
         {
             "id": "FacePositionY",
-            "value": ifacial_data[ifacial.HEAD_POSITION_Y] * POSITION_Y_RATIO
+            "value": ifacial_data[ifacial.HEAD_POSITION_Y] * FACE_POSITION_Y_RATIO
         },
         {
             "id": "FacePositionZ",
-            "value": -ifacial_data[ifacial.HEAD_POSITION_Z] * POSITION_Z_RATIO
+            "value": -ifacial_data[ifacial.HEAD_POSITION_Z] * FACE_POSITION_Z_RATIO
         },
         {
             "id": "FaceAngleX",
-            "value": ifacial_data[ifacial.HEAD_ROTATION_Y]
+            "value": ifacial_data[ifacial.HEAD_ROTATION_Y] * FACE_ANGLE_X_RATIO
         },
         {
             "id": "FaceAngleY",
-            "value": -ifacial_data[ifacial.HEAD_ROTATION_X]
+            "value": -ifacial_data[ifacial.HEAD_ROTATION_X] * FACE_ANGLE_Y_RATIO
         },
         {
             "id": "FaceAngleZ",
-            "value": -ifacial_data[ifacial.HEAD_ROTATION_Z]
+            "value": -ifacial_data[ifacial.HEAD_ROTATION_Z] * FACE_ANGLE_Z_RATIO
         },
         {
             "id": "MouthSmile",
             "value": (
                         (
-                            (ifacial_data[ifacial.MOUTH_SMILE_LEFT] + ifacial_data[ifacial.MOUTH_SMILE_RIGHT]) / 2 * MOUSE_SMILE_RATIO  # mouth smile (pos)
-                            - ifacial_data[ifacial.MOUTH_SHRUG_LOWER] * MOUSE_SMILE_RATIO                                               # mouth shrug (neg)
-                        ) / 2 + 0.5 + MOUSE_SMILE_OFFSET                                                                                # ratio
-                        - (math.pow(ifacial_data[ifacial.JAW_OPEN], 0.1) * 0.5)             # mouth open correction (neg; sad component)
+                            (
+                                (ifacial_data[ifacial.MOUTH_SMILE_LEFT] + ifacial_data[ifacial.MOUTH_SMILE_RIGHT])      # mouth smile (pos)
+                                - ifacial_data[ifacial.MOUTH_SHRUG_LOWER]                                               # mouth shrug (neg)
+                            ) * MOUTH_SMILE_RATIO                                                                       # ratio
+                        ) / 2 + 0.5                                                                                     # range re-mapping
+                        - (math.pow(ifacial_data[ifacial.JAW_OPEN], 0.4) * 1)                                           # mouth open correction (neg; sad component)
                     )
         },
         {
@@ -52,7 +72,7 @@ def build_params_dict(ifacial_data):
         },
         {
             "id": "Brows",
-            "value": ifacial_data[ifacial.BROW_INNER_UP]
+            "value": ifacial_data[ifacial.BROW_INNER_UP] * BROWS_RATIO
         },
         {
             "id": "TongueOut",
@@ -68,39 +88,39 @@ def build_params_dict(ifacial_data):
         },
         {
             "id": "EyeLeftX",
-            "value": ifacial_data[ifacial.EYE_LOOK_IN_LEFT] - ifacial_data[ifacial.EYE_LOOK_OUT_LEFT]
+            "value": (ifacial_data[ifacial.EYE_LOOK_IN_LEFT] - ifacial_data[ifacial.EYE_LOOK_OUT_LEFT]) * EYE_ROTATION_RATIO
         },
         {
             "id": "EyeLeftY",
-            "value": ifacial_data[ifacial.EYE_LOOK_UP_LEFT] - ifacial_data[ifacial.EYE_LOOK_DOWN_LEFT]
+            "value": (ifacial_data[ifacial.EYE_LOOK_UP_LEFT] - ifacial_data[ifacial.EYE_LOOK_DOWN_LEFT]) * EYE_ROTATION_RATIO
         },
         {
             "id": "EyeRightX",
-            "value": ifacial_data[ifacial.EYE_LOOK_OUT_RIGHT] - ifacial_data[ifacial.EYE_LOOK_IN_RIGHT]
+            "value": (ifacial_data[ifacial.EYE_LOOK_OUT_RIGHT] - ifacial_data[ifacial.EYE_LOOK_IN_RIGHT]) * EYE_ROTATION_RATIO
         },
         {
             "id": "EyeRightY",
-            "value": ifacial_data[ifacial.EYE_LOOK_UP_RIGHT] - ifacial_data[ifacial.EYE_LOOK_DOWN_RIGHT]
+            "value": (ifacial_data[ifacial.EYE_LOOK_UP_RIGHT] - ifacial_data[ifacial.EYE_LOOK_DOWN_RIGHT]) * EYE_ROTATION_RATIO
         },
         {
             "id": "CheekPuff",
-            "value": 0
+            "value": ifacial_data[ifacial.CHEEK_PUFF] * CHEEK_PUFF_RATIO
         },
         {
             "id": "FaceAngry",
-            "value": 0
+            "value": ifacial_data[ifacial.MOUTH_ROLL_LOWER] * FACE_ANGRY_RATIO
         },
         {
             "id": "BrowLeftY",
-            "value": ifacial_data[ifacial.BROW_OUTER_UP_LEFT]
+            "value": ifacial_data[ifacial.BROW_OUTER_UP_LEFT] * BROW_LEFT_Y_RATIO
         },
         {
             "id": "BrowRightY",
-            "value": ifacial_data[ifacial.BROW_OUTER_UP_RIGHT]
+            "value": ifacial_data[ifacial.BROW_OUTER_UP_RIGHT] * BROW_RIGHT_Y_RATIO
         },
         {
             "id": "MouthX",
-            "value": 0
+            "value": (ifacial_data[ifacial.MOUTH_LEFT] - ifacial_data[ifacial.MOUTH_RIGHT]) * MOUTH_X_RATIO
         },
     ]
     return ifacial_data
